b776de1a4b9773b47404b8a2145304a0f469e9c4
Runtus <893119806@qq.com>
2020-03-05 21:27:19 +0800

    attend github OAuth and log in function

--- front-end/src/axios.js
+++ front-end/src/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios"
 
 // axios.defaults.baseURL = "http://127.0.0.1:8000";
-axios.defaults.baseURL = "http://118.31.105.159:3389";
+axios.defaults.baseURL = "http://118.31.105.159:8001";
 // axios.defaults = { withCredentials: true};
 //跨域字段处理
 axios.defaults.withCredentials = true;
